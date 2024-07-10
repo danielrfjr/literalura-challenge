edc32f5f9a0b7f699972707554a1bafc9b2535a8
danielrfjr <157657284+danielrfjr@users.noreply.github.com>
2024-07-10 12:05:04 -0300

    Create LanguageRepository.java

---
+++ LanguageRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.model.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+}
