616f2aa60526d663e90ec2a77ac668159db26bf0
danielrfjr <157657284+danielrfjr@users.noreply.github.com>
2024-07-10 11:57:38 -0300

    Create GutendexApplication.java

---
+++ GutendexApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutendexApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GutendexApplication.class, args);
+    }
+}
